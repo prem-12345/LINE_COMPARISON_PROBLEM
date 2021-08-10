a75c3fffe00ad799d31a6ca8c1f56e9e94081b32
prem-12345 <premkpatil1997@gmail.com>
2021-08-10 16:32:02 +0530

    UC1_CALCULATE_THE_LENGTH

--- src/com/prem01/linecomp/LINECOMPARISON.java
+++ src/com/prem01/linecomp/LINECOMPARISON.java
@@ -2,9 +2,35 @@ package com.prem01.linecomp;
 
 //WELCOME TO LINE COMPARISON COMPUTATION PROGRAMME
 
+/**
+ * UC1 MODEL A LINE BASED ON POINT CONSISTING OF (X,Y) CO-ORDINATES AND CALCULATE THE LENTH
+ *
+ * @author prem
+ * @version 2.1
+ * @since 10/08/2021
+ */
+
+import java.util.Scanner;
 
 public class LINECOMPARISON {
     public static void main(String[] args) {
-        System.out.println("WELCOME TO LINE COMPARISON COMPUTATION PROGRAMME");
+
+        int x1, x2, y1, y2;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter x1 point");
+        x1 = input.nextInt();
+        System.out.println("Enter x2 point");
+        x2 = input.nextInt();
+        System.out.println("Enter y1 point");
+        y1 = input.nextInt();
+        System.out.println("Enter y2 point");
+        y2 = input.nextInt();
+
+        double length = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length of the line between points " + "(" + x1 + "," + y1 + "),(" + x2 + "," + y2 + ") = " + length);
+
+
     }
 }
