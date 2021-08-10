8eb7093c14f32d1bfa4e2ed7c72a8fe120f848e1
prem-12345 <85114766+prem-12345@users.noreply.github.com>
2021-08-10 18:11:47 +0530

    Update LINECOMPARISON.java

--- src/com/prem01/linecomp/LINECOMPARISON.java
+++ src/com/prem01/linecomp/LINECOMPARISON.java
@@ -14,9 +14,12 @@ import java.util.Scanner;
 
 public class LINECOMPARISON {
     public static void main(String[] args) {
+        
+       //taking variables for co-ordinates of line 1
 
         int x1, x2, y1, y2;
 
+        //taking the user inputs for co-ordinates of line 1
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter coordinate of line-1\n" +
@@ -29,10 +32,13 @@ public class LINECOMPARISON {
         System.out.println("Enter y2 point");
         y2 = input.nextInt();
 
+        //computing the length of line 1
         double length1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 
+        //taking variables for co-ordinates of line 2
         int x3, x4, y3, y4;
 
+        //taking the user inputs for co-ordinates of line 2
         Scanner input2 = new Scanner(System.in);
 
         System.out.println("Enter coordinate of line-2\n" +
@@ -45,11 +51,14 @@ public class LINECOMPARISON {
         System.out.println("Enter y4 point");
         y4 = input2.nextInt();
 
+        //computing the length of line 2
         double length2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
 
+        //printing the lengths of lines
         System.out.println("Length of line-1 " + "(" + x1 + "," + y1 + "),(" + x2 + "," + y2 + ") = " + length1);
         System.out.println("Length of line-2 " + "(" + x3 + "," + y3 + "),(" + x4 + "," + y4 + ") = " + length2);
 
+        //comparing the lengths of lines
         if (length1 == length2) {
             System.out.println("Line-1 and Line-2 are equal");
         }
