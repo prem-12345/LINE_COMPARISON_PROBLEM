5870714e41487506f605052773478106391e33db
prem-12345 <premkpatil1997@gmail.com>
2021-08-10 15:48:54 +0530

    LINE_COMPARISON_PROGRAMME

---
+++ src/com/prem01/linecomp/LINECOMPARISON.java
@@ -0,0 +1,10 @@
+package com.prem01.linecomp;
+
+//WELCOME TO LINE COMPARISON COMPUTATION PROGRAMME
+
+
+public class LINECOMPARISON {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO LINE COMPARISON COMPUTATION PROGRAMME");
+    }
+}
